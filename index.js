969c3eaf6c9c292c2d1f606986d7f2acce28b922
Shivam singh <shivam.singhcs2020@indoreinstitute.com>
2023-07-17 23:29:12 +0530

    navbar responsive done

---
+++ index.js
@@ -0,0 +1,15 @@
+// const navbar = document.querySelector(".navbar");
+// const navItems = document.querySelector(".nav-items");
+// const menuBar = document.getElementById("#menu-bar")
+
+// const openNavItem = () => {
+//     navbar.classList.add("active");
+//     navItems.classList.add("active");
+//     menuBar.classList.remove("#active")
+    
+// };
+
+// const closeNavItem = () => {
+//     navbar.classList.add("active");
+//      navItems.classList.add("active");
+// };
