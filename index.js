30bab2ab6269bae5d71a0bd4294ef909123a39fb
Shivam singh <shivam.singhcs2020@indoreinstitute.com>
2023-07-19 00:07:21 +0530

    skills and education section added

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-// const navbar = document.querySelector(".navbar");
-// const navItems = document.querySelector(".nav-items");
-// const menuBar = document.getElementById("#menu-bar")
+// skill section
 
-// const openNavItem = () => {
-//     navbar.classList.add("active");
-//     navItems.classList.add("active");
-//     menuBar.classList.remove("#active")
-    
-// };
+const skillsContent = document.getElementsByClassName('skill_content'),
+    skillsHeader = document.querySelectorAll('.skill_header') 
+       
 
-// const closeNavItem = () => {
-//     navbar.classList.add("active");
-//      navItems.classList.add("active");
-// };
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+    for (i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skill_content skills_close'
+
+    }
+
+    if (itemClass === 'skill_content skills_close') {
+        this.parentNode.className = 'skill_content skills_open'
+    }
+}
+
+    skillsHeader.forEach((el) => {
+        el.addEventListener('click',toggleSkills)
+    })
+
+
+
+
+    // qualification section
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+        
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+
+        target.classList.add('qualification_active')
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+        })
+    })
